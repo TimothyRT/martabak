4ca27f8d6efdf3a8cccf41242ecdbe1956fd52bb
TimothyRT <timothyrtzion@gmail.com>
2024-03-08 02:47:52 +0700

    alpha_version

---
+++ reveal.js
@@ -0,0 +1,21 @@
+ScrollReveal({ reset: false });
+
+// Home
+ScrollReveal().reveal('.carousel', { easing:"ease-in", delay: 100});
+ScrollReveal().reveal('.home-heading', { easing:"ease-in", delay: 100});
+ScrollReveal().reveal('.main-content-image', { easing:"ease-in", delay: 100});
+ScrollReveal().reveal('.contact', { easing:"ease-in", delay: 100});
+
+// Menu
+ScrollReveal().reveal('.container-manis', { easing:"ease-in", delay: 100});
+ScrollReveal().reveal('.container-asin', { easing:"ease-in", delay: 100});
+ScrollReveal().reveal('.container-minuman', { easing:"ease-in", delay: 100});
+
+// Menu - Detail
+ScrollReveal().reveal('.detail-menu-makanan', { easing:"ease-in", delay: 100});
+
+// Rewards
+ScrollReveal().reveal('.rewards-main-container', { easing:"ease-in", delay: 100});
+
+// Stores
+ScrollReveal().reveal('.stores-header', { easing:"ease-in", delay: 100});
